011a285f09c5daa0c0e0f689e61514c30edc0efb
dighin <58272091+dighin@users.noreply.github.com>
2022-12-30 16:10:03 +0900

    Update menu_hd_underground.tsx

--- pages/menu_hd_underground.tsx
+++ pages/menu_hd_underground.tsx
@@ -7,7 +7,7 @@ import Unsplash from 'components/metrics/Unsplash';
 import YouTube from 'components/metrics/Youtube';
 import TopTracks from 'components/TopTracks';
 
-import { useState, useRef, useEffect } from 'react';
+import { useState, useEffect } from 'react';
  
 export default function Menu_hd_underground() {
 
