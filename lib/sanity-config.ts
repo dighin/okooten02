e25b7207c760fd537498e0f918f1bd8b54d9aa84
dighin <58272091+dighin@users.noreply.github.com>
2022-12-30 17:20:51 +0900

    Update sanity-config.ts

--- lib/sanity-config.ts
+++ lib/sanity-config.ts
@@ -1,5 +1,5 @@
 export const sanityConfig = {
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  dataset: 'production',
   projectId: 'okooten',
   useCdn: process.env.NODE_ENV !== 'production',
   apiVersion: '2021-03-25'
